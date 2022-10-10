33eb0917524d46c9b50daea7f883b447a1f69bad
Ashlynn Landry <ashclandry@gmail.com>
2022-10-10 17:55:50 -0400

    working app

--- routes/api.js
+++ routes/api.js
@@ -9,30 +9,30 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
   });
 
-  // POST new note, add to the db.json file & return to the client. 
+  // POST /api/notes new note, add to the db.json file, and return to the client. 
   app.post('/api/notes', (req, res) => {
-    let dataBase = fs.readFileSync('db/db.json');
-    dataBase = JSON.parse(dataBase);
-    res.json(dataBase);
-    // body for note
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
     let userNote = {
       title: req.body.title,
       text: req.body.text,
       id: uniqid(),
     };
-    // push note to db.json file
     db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(dataBase));
-    res.json(dataBase);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
 
   });
 
+
   // DELETE 
   app.delete('/api/notes/:id', (req, res) => {
-    let dataBase = JSON.parse(fs.readFileSync('db/db.json'))
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
     // removing note with id
     let deleteNotes = db.filter(item => item.id !== req.params.id);
-
+    
     fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
     res.json(deleteNotes);
     
